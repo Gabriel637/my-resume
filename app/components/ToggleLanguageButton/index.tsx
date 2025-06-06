e915268f9ae041c02ce88e2d27f589cd78cb521f
Gabriel Barbosa <gabriel@Gabriels-MacBook-Air.local>
2025-06-06 10:11:26 -0300

    create header, add change theme and language

---
+++ app/components/ToggleLanguageButton/index.tsx
@@ -0,0 +1,20 @@
+import { useTheme } from '../../contexts/ThemeContext';
+import { useI18n, type SupportedLanguage } from "~/contexts/i18nContext";
+
+const ToggleLanguageButton = () => {
+  const { theme } = useTheme();
+  const { language, setLanguage } = useI18n();
+
+  return (
+    <select
+      defaultValue={language}
+      onChange={(e) => setLanguage(e.currentTarget.value as SupportedLanguage)} className="select"
+    >
+      <option value="en">EN</option>
+      <option value="pt">PT</option>
+      <option value="es">ES</option>
+    </select>
+  )
+}
+
+export default ToggleLanguageButton;
